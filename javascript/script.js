deb77cd291a3b9c8ab2fc372f7aa232b97e38e36
Alberto Defendi <133432639+albertodefendi@users.noreply.github.com>
2023-05-17 17:15:14 +0200

    Completed portfolio

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+// Hamburger menu switch
+function switchHamburger(x) {
+    x.classList.toggle("change");
+}
+function switchHamburger() {
+    document.getElementById("hamburger").classList.toggle("change");
+}
