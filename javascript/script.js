06df838f11e3afd5e47da545f26c69744e208b04
Alberto Defendi <133432639+albertodefendi@users.noreply.github.com>
2024-05-19 18:35:26 +0200

    Completato stile del form

--- javascript/script.js
+++ javascript/script.js
@@ -13,7 +13,39 @@ function switchHamburger()
 
 
 
+let sendFormButton = document.getElementById("send-button")
+let sendFormButtonContainer = document.getElementById("send-button-container")
 let resetFormButton = document.getElementById("reset-button")
+let resetFormButtonContainer = document.getElementById("reset-button-container")
+
 resetFormButton.addEventListener("click", () => {
     document.getElementById("email-form").reset();
+})
+
+sendFormButton.addEventListener("mouseover", () => {
+    sendFormButtonContainer.style.width = "69.5%";
+    sendFormButtonContainer.style.transitionDuration = "200ms";
+    resetFormButtonContainer.style.width = "29.5%";
+    resetFormButtonContainer.style.transitionDuration = "200ms";
+})
+
+resetFormButton.addEventListener("mouseover", () => {
+    sendFormButtonContainer.style.width = "29.5%";
+    sendFormButtonContainer.style.transitionDuration = "200ms";
+    resetFormButtonContainer.style.width = "69.5%";
+    resetFormButtonContainer.style.transitionDuration = "200ms";
+})
+
+sendFormButton.addEventListener("mouseleave", () => {
+    sendFormButtonContainer.style.width = "49.5%";
+    sendFormButtonContainer.style.transitionDuration = "200ms";
+    resetFormButtonContainer.style.width = "49.5%";
+    resetFormButtonContainer.style.transitionDuration = "200ms";
+})
+
+resetFormButton.addEventListener("mouseleave", () => {
+    sendFormButtonContainer.style.width = "49.5%";
+    sendFormButtonContainer.style.transitionDuration = "200ms";
+    resetFormButtonContainer.style.width = "49.5%";
+    resetFormButtonContainer.style.transitionDuration = "200ms";
 })
